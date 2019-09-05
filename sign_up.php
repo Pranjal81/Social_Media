3b8cddb131b9b6342e77641c5ebb4016ac11fef0
Pranjal81 <pranjalbansal81@gmail.com>
2019-09-05 11:26:19 +0530

    insert query is not working

--- sign_up.php
+++ sign_up.php
@@ -1,16 +1,18 @@
 <?php
 	$conn=mysqli_connect('localhost','root','','social_media');
-	if(isset($conn))
-	{
+	if($conn)
+	{	echo "conn";
 		if(isset($_POST['submit']))
-		{
+		{	echo "all set";
 			$unique="SELECT * FROM record WHERE Username='$_POST[username]'";
 			$fire=mysqli_query($conn,$unique); 
 			if(mysqli_num_rows($fire)==0)
-			{
-				$in="INSERT INTO record (First_Name,Last_Name,E-mail,Mobile_No.,Gender,Username,Password) 	VALUES ('$_POST[fname]','$_POST[lname]','$_POST[mail]','$_POST[phone]','$_POST[gender]','$_POST[username]','$_POST[pass]')";
+			{echo "done";
+				$in="INSERT INTO record (First_Name,E-mail,Mobile_No.,Gender,Username,Password) 
+					VALUES ('$_POST[fname]', '$_POST[mail]', '$_POST[phone]', '$_POST[gender]', '$_POST[username]','$_POST[pass]')";
 				mysqli_query($conn,$in);
+				echo "help";
 			}
 		}
 	}
-?>
+// ?>
