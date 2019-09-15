12b4cb1327b46ad40d9b0e80e2de6975ccaf5127
wolf0002 <54803618+wolf0002@users.noreply.github.com>
2019-09-15 11:29:32 +0530

    4 table database

--- social_media.sql
+++ social_media.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2019 at 07:51 PM
+-- Generation Time: Sep 15, 2019 at 07:58 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -25,22 +25,127 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `record`
+-- Table structure for table `comments`
 --
 
-CREATE TABLE `record` (
-  `First_Name` text NOT NULL,
-  `Last_Name` text NOT NULL,
-  `D.O.B` date NOT NULL,
-  `E-mail` text NOT NULL,
-  `Mobile_No.` int(10) NOT NULL,
-  `Gender` text NOT NULL,
-  `Country` text NOT NULL,
-  `State` text NOT NULL,
-  `City` text NOT NULL,
-  `Username` text NOT NULL,
-  `Password` text NOT NULL
+CREATE TABLE `comments` (
+  `comment_id` int(100) NOT NULL,
+  `post_id` varchar(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `content_comment` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `created` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE `photos` (
+  `photo_id` int(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `date_added` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `post_id` int(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `post_image` varchar(100) NOT NULL,
+  `content` varchar(100) NOT NULL,
+  `created` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `birthday` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `profile_picture` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `phone`, `birthday`, `gender`, `email`, `password`, `profile_picture`) VALUES
+(7, 'ankur', 'patel', 'ankur', '8429355953', '', 'Male', 'sadgt@gmail.com', '2119487c1b96840f529312aba95c6a783ddb8f65', 'ankur_pic.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`photo_id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `photo_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `post_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
