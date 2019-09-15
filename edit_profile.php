01e137a8d88f39cb41aba1d41d50368912c73a1c
wolf0002 <54803618+wolf0002@users.noreply.github.com>
2019-09-15 22:58:25 +0530

    edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -3,68 +3,6 @@
 include 'connection.php';
 include 'session.php';
 
-if(isset($_POST['save']))
-{
-  $_SESSION['fnameErr']=$_SESSION['lnameErr']=$_SESSION['phoneErr']=$_SESSION['genderErr']=$_SESSION['mailErr']="";
-    $_SESSION['error']="";
-    $var1=0;
-    if(empty($_POST['fname'])) {
-        $_SESSION['fnameErr']="Firstname can not be empty!";
-        $var1=1;
-    }
-    if(empty($_POST['phone'])) {
-        $_SESSION['phoneErr']="Phone no. can not be empty!";
-        $var1=1;
-    }
-    if(empty($_POST['gender'])) {
-        $_SESSION['genderErr']="Please select a gender";
-        $var1=1;
-    }
-    if(empty($_POST['mail'])) {
-        $_SESSION['mailErr']="E-mail can not be empty!";
-        $var1=1;
-    }
-    if($var1==1) {
-        header("location:edit_profile.php");
-    }
-
-    else{
-
-    $fname=mysqli_real_escape_string($conn, $_POST['fname']);
-    $lname=mysqli_real_escape_string($conn, $_POST['lname']);
-    $phone=mysqli_real_escape_string($conn, $_POST['phone']);
-    $gender=mysqli_real_escape_string($conn,$_POST['gender']);
-    $mail=mysqli_real_escape_string($conn, $_POST['email']);
-    $bday=$_POST['birthday'];
-
-    $var=0;
-    if(!preg_match("/^[a-zA-Z ]*$/",$fname)) {
-        $_SESSION['fnameErr']="Only letters and white space allowed!"; $var=1; }
-    if(!preg_match("/^[a-zA-Z ]*$/",$lname)) {
-        $_SESSION['lnameErr']="Only letters and white space allowed!"; $var=1; }
-    if(!preg_match("/^[0-9 ]*$/",$phone)) {
-        $_SESSION['phoneErr']="Only numerals allowed!"; $var=1; }
-    if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",  $mail)) {
-      $_SESSION['mailErr']="E-mail is not valid!"; $var=1; }
-
-    if($var==1){
-        header("location:edit_profile.php");
-    }
-
-    else{
-    $sqr="UPDATE `user` SET firstname='$fname', lastname='$lname', phone='$phone' birthday='$bday', gender='$gender', email='$mail' WHERE user_id='$user_id'";
-    $result= mysqli_query($conn, $sqr) or die(mysqli_error($conn));
-    if($result) {
-      $_SESSION['scs']= "Successfully saved :)";
-      header("location:profile_page.php");
-    }
-    else {
-      $_SESSION['err']= "Something is wrong, please try again later!";
-      header("location:profile_page.php");
-    }
-}
-}
-}
 ?>
 
 <!DOCTYPE html>
@@ -76,7 +14,7 @@ ul.horizontal {
   margin: 0;
   padding: 1.5%;
   overflow: hidden;
-  background-color: #534;
+  background-color: #333;
 }
 ul.vertical {
   list-style-type: none;
@@ -111,14 +49,14 @@ li a:hover:not(.active) {
   background-color: #111;
 }
 .active {
-  background-color: #86DF20;
+  background-color: #4CAF50;
 }
 #error
     {
       color: red;
       font-size: 70%;
     }
-input{
+    input {
   height: 40px;
   width: 20%;
   border-color: #534;
@@ -126,38 +64,48 @@ input{
   border-radius: 3px;
   margin-top: 7px;
   border-radius: 5px;
-
+  font-size: 15px;
 }
 </style>
 </head>
-<body bgcolor="lightgrey" background="bg2.png">
+<body>
 
 <ul class="horizontal">
   <li class="list1"><img src="<?php echo $img_location; ?>" alt="Avatar" style="width: 60px; height: 50px;margin: 0;padding: 0;border-radius: 50%;padding-right: 5px;"></li>
   <li class="list1"><a href="home.php">Home</a></li>
   <li class="list1"><a href="profile_page.php">Profile</a></li>
-  <li class="list1"><a class="active" href="#edit">Edit</a></li>
-  <li class="list1"><a href="contact.html">Friend List</a></li>
-  <li class="list1"><a href="photos.html">My Photos</a></li>
+  <li class="list1"><a href="#contact">Friend List</a></li>
+    <li class="list1"><a href="#contact">My Photos</a></li>
   <li class="list1" style="float:right"><a href="logout.php">Logout</a></li>
 </ul>
-<p id="edit">
-<form action="edit_profile.php" method="POST" style="font-size: 25px;text-align: center;"><b>
+
+<div style="float: right;">
+<img src="<?php echo $img_location; ?>"alt="Profile picture"><br>
+<b>Change profile picture</b>
+<form action="profile_photo.php" method="POST">
+ <input type="file" name="edit_profile_photo" placeholder="Edit Profile Photo"><br>
+ <input type="submit" name="change_profile_photo" value="Change Profile Picture">
+</form>
+</div>
+
+<form action="edit_profilephp.php" method="POST">
   <label for="fname">Firstname:</label>
   <input type="text" id="fname" name="fname" value="<?php echo $firstname; ?>" autofocus><span id="error">*<?php echo $_SESSION['fnameErr']; $_SESSION['fnameErr']=""; ?></span><br>
   <label for="lname">Lastname:</label>
-  <input type="text" id="lname" name="lname" value="<?php echo $lastname; ?>"><span id="error">*<?php echo $_SESSION['lnameErr']; $_SESSION['lnameErr']=""; ?></span><br>
+  <input type="text" id="lname" name="lname" value="<?php echo $lastname; ?>"><span id="error"><?php echo $_SESSION['lnameErr']; $_SESSION['lnameErr']=""; ?></span><br>
   <label for="phone">Phone No.:</label>
   <input type="text" id="phone" name="phone" value="<?php echo $phone; ?>"><span id="error">*<?php echo $_SESSION['phoneErr']; $_SESSION['phoneErr']=""; ?></span><br>
   <label for="bday" >Birthday:</label>
   <input type="date" id="bday" name="birthday" value="<?php echo $birthday; ?>"><br>
   <label>Gender:</label>
-  <input type="radio" id="male" name="gender" value="<?php echo $gender; ?>" style="width: 5%;">Male
-  <input type="radio" id="female" name="gender" value="<?php echo $gender; ?>" style="width: 5%;">Female<br>
+  <input type="radio" id="male" name="gender" value="<?php echo $gender; ?>">
+  <label for="male">Male</label>
+  <input type="radio" id="female" name="gender" value="<?php echo $gender; ?>">
+  <label for="female">Female</label><span id="error">*<?php echo $_SESSION['genderErr']; $_SESSION['genderErr']=""; ?></span><br>
   <label for="email">E-mail:</label>
-  <input type="text" id="email" name="email" value="<?php echo $mail; ?>"><span id="error">*<?php echo $_SESSION['emailErr']; $_SESSION['emailErr']=""; ?></span><br><br>
-  <input type="submit" name="save" value="Save" style="background-color:#534;width: 7%;">
-</form></b>
-</p>
+  <input type="text" id="email" name="email" value="<?php echo $mail; ?>"><span id="error">*<?php echo $_SESSION['emailErr']; $_SESSION['emailErr']=""; ?></span><br>
+  <input type="submit" name="save" value="Save">
+</form>
 </body>
 </html>
+
